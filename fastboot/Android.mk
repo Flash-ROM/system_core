f7d43ded1adb08dd57de97910a3450df99e55d4e
Keith Mok <kmok@cyngn.com>
2015-09-28 15:03:06 -0700

    fastboot: format f2fs partition instead of just erase
    When doing fastboot -w on Darwin for f2fs partitions,
    it only erase it without format it due to a flag not set in Android.mk
    
    Change-Id: I642672467d68c75915900bb88c9aa86571140e03

--- fastboot/Android.mk
+++ fastboot/Android.mk
@@ -69,6 +69,7 @@ LOCAL_STATIC_LIBRARIES := \
     libcutils \
     libgtest_host \
 
+ifneq (,$(filter linux darwin,$(HOST_OS)))
 # libf2fs_dlutils_host will dlopen("libf2fs_fmt_host_dyn")
 LOCAL_CFLAGS_linux := -DUSE_F2FS
 LOCAL_LDFLAGS_linux := -ldl -rdynamic -Wl,-rpath,.
@@ -87,7 +88,7 @@ LOCAL_SHARED_LIBRARIES :=
 include $(BUILD_HOST_EXECUTABLE)
 
 my_dist_files := $(LOCAL_BUILT_MODULE)
-ifeq ($(HOST_OS),linux)
+ifneq (,$(filter linux darwin,$(HOST_OS)))
 my_dist_files += $(HOST_LIBRARY_PATH)/libf2fs_fmt_host_dyn$(HOST_SHLIB_SUFFIX)
 endif
 $(call dist-for-goals,dist_files sdk win_sdk,$(my_dist_files))
